70eb1ec1b65c24e757d4a0bf4c72c4d2238008e9
Ashfaqbs <ash001x@gmail.com>
2023-12-25 22:35:05 +0530

    General Commits

--- EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/EmsBackendApplication.java
+++ EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/EmsBackendApplication.java
@@ -3,6 +3,7 @@ package com.ashfaq.emsbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// This code snippet is the entry point for a Spring Boot application. It uses the @SpringBootApplication annotation to configure and start the application. The main method runs the application by calling SpringApplication.run().
 @SpringBootApplication
 public class EmsBackendApplication {
 
