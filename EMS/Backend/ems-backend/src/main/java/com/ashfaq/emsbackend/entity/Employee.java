debaa3fb4e86f591008f1dae2545155aad33eaa9
Ashfaqbs <ash001x@gmail.com>
2023-12-26 18:36:01 +0530

    General Commits

--- EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/entity/Employee.java
+++ EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/entity/Employee.java
@@ -1,5 +1,15 @@
 package com.ashfaq.emsbackend.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Employee {
 
     private long id;
