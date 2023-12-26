4c5cd88b684abfc01086e5fc460be876b7d5aca3
Ashfaqbs <ash001x@gmail.com>
2023-12-26 20:02:19 +0530

    General Commits

--- EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/controller/EmployeeController.java
+++ EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,13 +18,13 @@ public class EmployeeController {
     private IEmployeeService employeeService;
 
     //Autowiring with the constructor
-    public EmployeeController(IEmployeeService employeeService) {
-        this.employeeService = employeeService;
-    }
+//    public EmployeeController(IEmployeeService employeeService) {
+//        this.employeeService = employeeService;
+//    }
 
     //Build Add Employee REST API
     @PostMapping("")
-    public ResponseEntity<EmployeeDTO>creteEmployee(EmployeeDTO employeeDTO)
+    public ResponseEntity<EmployeeDTO>creteEmployee(@RequestBody EmployeeDTO employeeDTO)
     {
        EmployeeDTO employeeDTO1=  employeeService.createEmployee(employeeDTO);
         return new ResponseEntity(employeeDTO1, HttpStatus.CREATED);
