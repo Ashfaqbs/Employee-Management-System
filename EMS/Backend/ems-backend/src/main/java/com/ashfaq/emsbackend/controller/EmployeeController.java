7bf36a2a9b28f1bb69c80fadb66b60664711d624
Ashfaqbs <ash001x@gmail.com>
2023-12-26 19:51:15 +0530

    General Commits

--- EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/controller/EmployeeController.java
+++ EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/controller/EmployeeController.java
@@ -3,7 +3,7 @@ package com.ashfaq.emsbackend.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-@RequestMapping("api/v1/empl")
+@RequestMapping("api/v1/employees")
 public class EmployeeController {
 
 
