cd16ace79d29c73ebd75e58c2e02f600a907a3cb
Ashfaqbs <ash001x@gmail.com>
2023-12-26 19:35:22 +0530

    General Commits

--- EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/sevices/IEmployeeService.java
+++ EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/sevices/IEmployeeService.java
@@ -1,10 +1,11 @@
 package com.ashfaq.emsbackend.sevices;
 
 import com.ashfaq.emsbackend.dto.EmployeeDTO;
+import com.ashfaq.emsbackend.entity.Employee;
 
 public interface IEmployeeService {
 
 
-    EmployeeDTO createEmployee()
+    EmployeeDTO createEmployee(Employee employee);
 
 }
