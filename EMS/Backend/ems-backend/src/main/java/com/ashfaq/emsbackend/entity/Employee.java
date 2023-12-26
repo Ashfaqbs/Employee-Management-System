2099ddb47bea2216f104025dea0165eaf714d9a0
Ashfaqbs <ash001x@gmail.com>
2023-12-26 18:54:00 +0530

    General Commits

--- EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/entity/Employee.java
+++ EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/entity/Employee.java
@@ -19,7 +19,7 @@ public class Employee {
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
-
+    @Column(name = "email_id")
     private String email;
 
 }
