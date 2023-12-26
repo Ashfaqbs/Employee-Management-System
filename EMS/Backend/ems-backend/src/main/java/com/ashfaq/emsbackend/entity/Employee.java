756230fa368edc2276bde93f73d7ce484d31ba63
Ashfaqbs <ash001x@gmail.com>
2023-12-26 18:34:34 +0530

    General Commits

--- EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/entity/Employee.java
+++ EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/entity/Employee.java
@@ -3,6 +3,7 @@ package com.ashfaq.emsbackend.entity;
 public class Employee {
 
     private long id;
-    private sString firstName;
-    private String  
+    private String firstName;
+    private String  lastName;
+
 }
