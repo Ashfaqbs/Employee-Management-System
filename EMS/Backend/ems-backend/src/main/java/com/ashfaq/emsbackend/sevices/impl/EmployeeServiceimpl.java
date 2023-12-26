251d25553e0f2b31fb1294fc417b6671db00ed87
Ashfaqbs <ash001x@gmail.com>
2023-12-26 19:37:38 +0530

    General Commits

--- EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/sevices/impl/EmployeeServiceimpl.java
+++ EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/sevices/impl/EmployeeServiceimpl.java
@@ -4,9 +4,10 @@ import com.ashfaq.emsbackend.dto.EmployeeDTO;
 import com.ashfaq.emsbackend.entity.Employee;
 import com.ashfaq.emsbackend.sevices.IEmployeeService;
 
+@
 public class EmployeeServiceimpl implements IEmployeeService {
 
-        
+
 
     @Override
     public EmployeeDTO createEmployee(Employee employee) {
