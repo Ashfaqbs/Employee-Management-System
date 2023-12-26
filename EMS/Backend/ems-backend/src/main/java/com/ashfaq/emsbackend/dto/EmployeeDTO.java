f01a18f923fd2a5e9abf0e36fe64e5483c58c83e
Ashfaqbs <ash001x@gmail.com>
2023-12-26 19:09:21 +0530

    General Commits

--- EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/dto/EmployeeDTO.java
+++ EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/dto/EmployeeDTO.java
@@ -1,4 +1,11 @@
 package com.ashfaq.emsbackend.dto;
 
+import jakarta.persistence.Column;
+
 public class EmployeeDTO {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
 }
