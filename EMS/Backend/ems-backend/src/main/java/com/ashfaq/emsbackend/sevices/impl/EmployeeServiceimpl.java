a308aaa806aac1f3f41e0f6f3dc1929cd8bc73f8
Ashfaqbs <ash001x@gmail.com>
2023-12-26 19:39:22 +0530

    General Commits

--- EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/sevices/impl/EmployeeServiceimpl.java
+++ EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/sevices/impl/EmployeeServiceimpl.java
@@ -2,13 +2,18 @@ package com.ashfaq.emsbackend.sevices.impl;
 
 import com.ashfaq.emsbackend.dto.EmployeeDTO;
 import com.ashfaq.emsbackend.entity.Employee;
+import com.ashfaq.emsbackend.repository.EmployeeRepository;
 import com.ashfaq.emsbackend.sevices.IEmployeeService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeServiceimpl implements IEmployeeService {
 
+private EmployeeRepository employeeRepository;
 
+    public EmployeeServiceimpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 
     @Override
     public EmployeeDTO createEmployee(Employee employee) {
