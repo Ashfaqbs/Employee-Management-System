1ad78e323d6265149e1521a5d0eebf2bb9bbde8c
Ashfaqbs <ash001x@gmail.com>
2023-12-27 19:50:15 +0530

    General Commits

--- EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/sevices/impl/EmployeeServiceimpl.java
+++ EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/sevices/impl/EmployeeServiceimpl.java
@@ -42,7 +42,7 @@ public class EmployeeServiceimpl implements IEmployeeService {
     public List<EmployeeDTO> getAllEmployee() {
       List<Employee> employees=  employeeRepository.findAll();
 
-        return employees.parallelStream().map(data -> EmployeeMapper.mapToEmployeeDTO(data)).toList();
+        return employees.parallelStream().map(EmployeeMapper::mapToEmployeeDTO).toList();
     }
 
 
