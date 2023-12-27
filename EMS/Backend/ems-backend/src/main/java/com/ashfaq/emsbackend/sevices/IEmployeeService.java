df627f4b5dc690f43c7bdd27f412c4c9fab9909b
Ashfaqbs <ash001x@gmail.com>
2023-12-27 20:59:55 +0530

    General Commits

--- EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/sevices/IEmployeeService.java
+++ EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/sevices/IEmployeeService.java
@@ -14,6 +14,6 @@ public interface IEmployeeService {
 
     List<EmployeeDTO> getAllEmployee();
 
-    EmployeeDTO updateEmployee(EmployeeDTO employeeDTO,Long emplLong);
+    EmployeeDTO updateEmployee(EmployeeDTO employeeDTO,Long emplId);
 
 }
