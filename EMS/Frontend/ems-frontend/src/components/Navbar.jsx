58c59a41f6e57d4b7c2ed4a1b97308dda7e55d93
Ashfaqbs <ash001x@gmail.com>
2023-12-28 20:17:44 +0530

    General Commits

--- EMS/Frontend/ems-frontend/src/components/Navbar.jsx
+++ EMS/Frontend/ems-frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
   return (
     <>
     
-    <nav className="navbar navbar-expand-lg bg-light">
+    {/* <nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
     <a className="navbar-brand" href="#">Navbar</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,7 +27,23 @@ export const Navbar = () => {
       </ul>
     </div>
   </div>
-</nav>
+</nav> */}
+
+<Navbar collapseOnSelect expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </>
   )
 }
