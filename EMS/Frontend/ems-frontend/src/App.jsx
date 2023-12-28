38ccd811f88c95f1b82195251e201ac3804d9b21
Ashfaqbs <ash001x@gmail.com>
2023-12-28 20:16:06 +0530

    General Commits

--- EMS/Frontend/ems-frontend/src/App.jsx
+++ EMS/Frontend/ems-frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import HelloWorld from './components/HelloWorld'
+import { Navbar } from './components/Navbar'
 
 function App() {
 
   return (
     <>
+    <Navbar/>
     <HelloWorld/>
     </>
   )
