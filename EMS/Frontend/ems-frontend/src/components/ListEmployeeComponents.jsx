d9557d50148576d31a934d8e7362fd7fe5f29f2a
Ashfaqbs <ash001x@gmail.com>
2023-12-28 20:29:05 +0530

    General Commits

--- EMS/Frontend/ems-frontend/src/components/ListEmployeeComponents.jsx
+++ EMS/Frontend/ems-frontend/src/components/ListEmployeeComponents.jsx
@@ -34,17 +34,14 @@ const sampleData=[
       <td>Otto</td>
       <td>@mdo</td>
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+
+    {
+        sampleData.map(data => {
+return ()
+
+        }))
+    }
+   
   </tbody>
 </table>
     
