d55db69e704a5713e525fc1c1df0abddce072e46
Ashfaqbs <ash001x@gmail.com>
2023-12-28 20:28:07 +0530

    General Commits

--- EMS/Frontend/ems-frontend/src/components/ListEmployeeComponents.jsx
+++ EMS/Frontend/ems-frontend/src/components/ListEmployeeComponents.jsx
@@ -3,13 +3,52 @@ import React from 'react'
 const ListEmployeeComponent = () => {
 
 const sampleData=[
-    {
-        
-    }
-]
+    {"id": 1, "firstName": "John", "lastName": "Doe", "email": "john.doe@example.com"},
+    {"id": 2, "firstName": "Jane", "lastName": "Smith", "email": "jane.smith@example.com"},
+    {"id": 3, "firstName": "Bob", "lastName": "Johnson", "email": "bob.johnson@example.com"},
+    {"id": 4, "firstName": "Alice", "lastName": "Williams", "email": "alice.williams@example.com"},
+    {"id": 5, "firstName": "Charlie", "lastName": "Brown", "email": "charlie.brown@example.com"},
+    {"id": 6, "firstName": "Eva", "lastName": "Green", "email": "eva.green@example.com"},
+    {"id": 7, "firstName": "Frank", "lastName": "Miller", "email": "frank.miller@example.com"},
+    {"id": 8, "firstName": "Grace", "lastName": "Turner", "email": "grace.turner@example.com"},
+    {"id": 9, "firstName": "Henry", "lastName": "Davis", "email": "henry.davis@example.com"},
+    {"id": 10, "firstName": "Ivy", "lastName": "Clark", "email": "ivy.clark@example.com"}
+  ]
+  
 
   return (
-    <div>ListEmployeeComponents</div>
+    <>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+    
+    </>
   )
 }
 
