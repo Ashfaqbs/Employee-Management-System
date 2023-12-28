38010a1a0f33629b0f696619e5569566b61baf87
Ashfaqbs <ash001x@gmail.com>
2023-12-28 20:13:34 +0530

    General Commits

--- EMS/Frontend/ems-frontend/src/App.jsx
+++ EMS/Frontend/ems-frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import HelloWorld from './components/HelloWorld'
 
 function App() {
 
   return (
     <>
-    
+    <HelloWorld/>
     </>
   )
 }
