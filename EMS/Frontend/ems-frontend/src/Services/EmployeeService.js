22aa47b898e16f2a244d904f65d494d62ba641c5
Ashfaqbs <ash001x@gmail.com>
2023-12-29 22:25:57 +0530

    General Commits

--- EMS/Frontend/ems-frontend/src/Services/EmployeeService.js
+++ EMS/Frontend/ems-frontend/src/Services/EmployeeService.js
@@ -4,5 +4,5 @@ const BACKEND_Rest_API_BASE_URL = "http://localhost:8080/api/v1/employees";
 
 import React from "react";
 
-export const listEmployees = () => axios.get(BACKEND_Rest_API_BASE_URL)
+export const listEmployees = () => axios.get(BACKEND_Rest_API_BASE_URL);
 
