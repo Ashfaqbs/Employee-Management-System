64cfa4f81b9cc694cfe70503d3e216fce3962f12
Ashfaqbs <ash001x@gmail.com>
2023-12-30 21:58:41 +0530

    General Commits

--- EMS/Frontend/ems-frontend/src/components/Navbar.jsx
+++ EMS/Frontend/ems-frontend/src/components/Navbar.jsx
@@ -1,38 +1,26 @@
-// Navbar.js
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Add this line
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        Your Logo
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+      <a className="navbar-brand" href="#">Brand Name</a>
+      <button className="navbar-toggler" type="button" onClick={toggle}>
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
+
+      <div className={isOpen ? 'collapse navbar-collapse show' : 'collapse navbar-collapse'} id="navbarNav">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="#">
-              Home
-            </a>
+            <a className="nav-link" href="#">Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
-              About
-            </a>
+            <a className="nav-link" href="#">Link</a>
           </li>
-          {/* Add more menu items as needed */}
         </ul>
       </div>
     </nav>
