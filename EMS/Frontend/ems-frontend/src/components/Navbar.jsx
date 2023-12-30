7fda3ac70082d8d58b56a1df49fbbdf35964c30e
Ashfaqbs <ash001x@gmail.com>
2023-12-30 21:57:14 +0530

    General Commits

--- EMS/Frontend/ems-frontend/src/components/Navbar.jsx
+++ EMS/Frontend/ems-frontend/src/components/Navbar.jsx
@@ -1,20 +1,38 @@
+// Navbar.js
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Add this line
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Brand Name</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <a className="navbar-brand" href="#">
+        Your Logo
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
+        <ul className="navbar-nav ml-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only"></span></a>
+            <a className="nav-link" href="#">
+              Home
+            </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
+            <a className="nav-link" href="#">
+              About
+            </a>
           </li>
+          {/* Add more menu items as needed */}
         </ul>
       </div>
     </nav>
