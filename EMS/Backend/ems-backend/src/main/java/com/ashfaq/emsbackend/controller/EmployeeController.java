ae4ef788c4a8caa84e3cb95c29bc29626c9aa999
Ashfaqbs <ash001x@gmail.com>
2023-12-30 18:55:20 +0530

    General Commits

--- EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/controller/EmployeeController.java
+++ EMS/Backend/ems-backend/src/main/java/com/ashfaq/emsbackend/controller/EmployeeController.java
@@ -47,6 +47,21 @@ public class EmployeeController {
     //Note when we ere returning HttpStatus.Found i.e status code will be 302 and for the above get and getall emp API's
     //we are giving this and while calling from frontEnd , using fetch we were able to get the data and render in UI
     //but when using Axios and calling this we were not able to get the data and it we going in catch block and in error of catch block we were seeing like
+//error :
+//    AxiosError {message: 'Request failed with status code 302', name: 'AxiosError', config: {…}, request: XMLHttpRequest, response: {…}, …}
+//    config
+//:
+//    {transitional: {…}, adapter: Array(2), transformRequest: Array(1), transformResponse: Array(1), timeout: 0, …}
+//    message
+//:
+//        "Request failed with status code 302"
+//    name
+//:
+//        "AxiosError"
+//    req
+//    and to fix this we returned status code as OK , and data started rendering in UI , so AXIOS , need to be careful and need to share 200 status code from Backend
+
+
 
 
     //Build update employee Rest API
