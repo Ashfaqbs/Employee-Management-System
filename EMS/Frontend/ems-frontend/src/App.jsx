b0b65bbde0a975c25ecab53c358aef4ddd5d3f74
Ashfaqbs <ash001x@gmail.com>
2023-12-30 21:49:18 +0530

    General Commits

--- EMS/Frontend/ems-frontend/src/App.jsx
+++ EMS/Frontend/ems-frontend/src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import HelloWorld from './components/HelloWorld'
 import ListEmployeeComponent from './components/ListEmployeeComponents'
+import MyNavbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
+    <MyNavbar/>
     <HelloWorld/>
     <ListEmployeeComponent/>
     </>
