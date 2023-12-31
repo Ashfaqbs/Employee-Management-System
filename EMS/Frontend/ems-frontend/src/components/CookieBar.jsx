09dda0dc8794a30d055cfda056b909d21c7c5146
Ashfaqbs <ash001x@gmail.com>
2023-12-31 08:26:35 +0530

    General Commits

--- EMS/Frontend/ems-frontend/src/components/CookieBar.jsx
+++ EMS/Frontend/ems-frontend/src/components/CookieBar.jsx
@@ -1,40 +1,54 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Cookies from 'js-cookie';
+
 function CookieBar() {
-    const [isCookieSet, setCookie] = useState(Cookies.get("cookieConsent"));
-    const [showCookieBar, setShowCookieBar] = useState(true);
-   
-    // Function to handle accepting cookies
-    const handleAcceptCookies = () => {
-      Cookies.set("cookieConsent", true);
-      setCookie(true);
-      setShowCookieBar(false);
-    };
-   
-    // Function to handle rejecting cookies
-    const handleRejectCookies = () => {
-      Cookies.remove("cookieConsent");
-      setCookie(false);
-      setShowCookieBar(false);
-    };
-   
-    if (!showCookieBar) {
-      return null;
-    }
-   
-    return (
-      <div>
-        <div>
-          <p>
-            This website uses cookies to improve your experience. Do you accept
-            cookies?
-          </p>
-          <button onClick={handleAcceptCookies}>Accept</button>
-          <button onClick={handleRejectCookies}>Reject</button>
-        </div>
-        <sub>Cookie set: {isCookieSet ? <b>Yes</b> : <b>No</b>}</sub>
-      </div>
-    );
+   const [isCookieSet, setCookie] = useState(Cookies.get("cookieConsent"));
+   const [showCookieBar, setShowCookieBar] = useState(true);
+  
+   // Function to handle accepting cookies
+   const handleAcceptCookies = () => {
+     // Collect browser details
+     const userAgent = navigator.userAgent;
+     const currentLocation = window.location.href;
+     const historyLength = window.history.length;
+     // Collect username (replace with actual method of getting username)
+     const username = "username"; // replace this with actual method
+
+     // Print details to console
+     console.log(`User Agent: ${userAgent}`);
+     console.log(`Current Location: ${currentLocation}`);
+     console.log(`History Length: ${historyLength}`);
+     console.log(`Username: ${username}`);
+
+     Cookies.set("cookieConsent", true);
+     setCookie(true);
+     setShowCookieBar(false);
+   };
+  
+   // Function to handle rejecting cookies
+   const handleRejectCookies = () => {
+     Cookies.remove("cookieConsent");
+     setCookie(false);
+     setShowCookieBar(false);
+   };
+  
+   if (!showCookieBar) {
+     return null;
    }
-   
-   export default CookieBar;
+  
+   return (
+     <div>
+       <div>
+         <p>
+           This website uses cookies to improve your experience. Do you accept
+           cookies?
+         </p>
+         <button onClick={handleAcceptCookies}>Accept</button>
+         <button onClick={handleRejectCookies}>Reject</button>
+       </div>
+       <sub>Cookie set: {isCookieSet ? <b>Yes</b> : <b>No</b>}</sub>
+     </div>
+   );
+}
+
+export default CookieBar;
