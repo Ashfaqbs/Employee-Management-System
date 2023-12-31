bb8d4fa5bb80433cadc9adec58924960b5e728e2
Ashfaqbs <ash001x@gmail.com>
2023-12-31 08:17:10 +0530

    General Commits

--- EMS/Frontend/ems-frontend/src/App.jsx
+++ EMS/Frontend/ems-frontend/src/App.jsx
@@ -3,13 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import ListEmployeeComponent from './components/ListEmployeeComponents'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-
+import CookieBar from ''
 function App() {
 
   return (
     <>
       <Navbar />
       <ListEmployeeComponent />
+      <CookieBar/>
       <Footer />
     </>
   )
