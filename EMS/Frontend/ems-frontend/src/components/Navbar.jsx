23ab3fcd1ceb744a7dad765b8a7e7556a80ffd70
Ashfaqbs <ash001x@gmail.com>
2023-12-31 08:54:27 +0530

    General Commits

--- EMS/Frontend/ems-frontend/src/components/Navbar.jsx
+++ EMS/Frontend/ems-frontend/src/components/Navbar.jsx
@@ -1,50 +1,97 @@
+// import React, { useState } from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// const Navbar = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const toggle = () => setIsOpen(!isOpen);
+
+
+
+//   const [theme, setTheme] = useState('light');
+//   const toggleTheme = () => {
+//     if (theme === 'light') {
+      
+//       document.documentElement.setAttribute('data-bs-theme', 'dark');
+//       setTheme('dark');
+//     } else {
+//       document.documentElement.setAttribute('data-bs-theme', 'light');
+//       setTheme('light');
+//     }
+//   };
+
+
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//       <a className="navbar-brand" href="#">  EMS React And Springboot App</a>
+//       <button className="navbar-toggler" type="button" onClick={toggle}>
+//         <span className="navbar-toggler-icon"></span>
+//       </button>
+
+//       <div className={isOpen ? 'collapse navbar-collapse show' : 'collapse navbar-collapse'} id="navbarNav">
+//         <ul className="navbar-nav ml-auto">
+//           <li className="nav-item active">
+//             <a className="nav-link" href="#">Home</a>
+//           </li>
+//           <li className="nav-item">
+//             <a className="nav-link" href="#">About</a>
+//           </li>
+//           <li className="nav-item">
+//             <a className="nav-link" href="#">Contact Us</a>
+//           </li>
+//         </ul>
+//         <button  className="btn btn-outline-primary ms-auto btn-pill " onClick={toggleTheme}>Toggle Theme</button>
+//         <h6 style={{ margin: '0.5em' }}>{theme}</h6>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+
+
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+ const [isOpen, setIsOpen] = useState(false);
+ const toggle = () => setIsOpen(!isOpen);
 
-  const toggle = () => setIsOpen(!isOpen);
+ const [theme, setTheme] = useState('light');
+ const toggleTheme = () => {
+   if (theme === 'light') {
+     document.documentElement.setAttribute('data-bs-theme', 'dark');
+     setTheme('dark');
+   } else {
+     document.documentElement.setAttribute('data-bs-theme', 'light');
+     setTheme('light');
+   }
+ };
 
+ return (
+   <nav className="navbar navbar-expand-lg bg-body">
+     <a className="navbar-brand" href="#">EMS React And Springboot App</a>
+     <button className="navbar-toggler" type="button" onClick={toggle}>
+       <span className="navbar-toggler-icon"></span>
+     </button>
 
-
-  const [theme, setTheme] = useState('light');
-  const toggleTheme = () => {
-    if (theme === 'light') {
-      
-      document.documentElement.setAttribute('data-bs-theme', 'dark');
-      document.documentElement.setAttribute('data-bs-theme', 'dark');
-      setTheme('dark');
-    } else {
-      document.documentElement.setAttribute('data-bs-theme', 'light');
-      setTheme('light');
-    }
-  };
-
-
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">  EMS React And Springboot App</a>
-      <button className="navbar-toggler" type="button" onClick={toggle}>
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className={isOpen ? 'collapse navbar-collapse show' : 'collapse navbar-collapse'} id="navbarNav">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">About</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Contact Us</a>
-          </li>
-        </ul>
-        <button  className="btn btn-outline-primary ms-auto btn-pill " onClick={toggleTheme}>Toggle Theme</button>
-        <h6 style={{ margin: '0.5em' }}>{theme}</h6>
-      </div>
-    </nav>
-  );
+     <div className={isOpen ? 'collapse navbar-collapse show' : 'collapse navbar-collapse'} id="navbarNav">
+       <ul className="navbar-nav ml-auto">
+         <li className="nav-item active">
+           <a className="nav-link" href="#">Home</a>
+         </li>
+         <li className="nav-item">
+           <a className="nav-link" href="#">About</a>
+         </li>
+         <li className="nav-item">
+           <a className="nav-link" href="#">Contact Us</a>
+         </li>
+       </ul>
+       <button className="btn btn-outline-primary ms-auto btn-pill" onClick={toggleTheme}>Toggle Theme</button>
+       <h6 style={{ margin: '0.5em' }}>{theme}</h6>
+     </div>
+   </nav>
+ );
 };
 
 export default Navbar;
